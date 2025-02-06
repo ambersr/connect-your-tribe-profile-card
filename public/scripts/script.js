75a0d8cf3af77363f2691cf1d0656a16e552bd7d
ambersr <amber.schalker@hva.nl>
2025-02-06 17:58:13 +0100

    Code conventies uitgevoerd

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,9 +1,9 @@
-// Selecteer de knoppen en de wrapper van de flipkaart
+// Animatie flippen visitekaartje
+
 const flipCardBtnTurnToBack = document.getElementById('flip-card-btn-turn-to-back');
 const flipCardBtnTurnToFront = document.getElementById('flip-card-btn-turn-to-front');
 const flipCard = document.querySelector('.wrapper-flipkaart');
 
-// Voeg een event listener toe voor de 'turn to back' knop
 flipCardBtnTurnToBack.addEventListener('click', () => {
   flipCard.classList.add('flipped');  // Voeg de 'flipped' class toe
 });
